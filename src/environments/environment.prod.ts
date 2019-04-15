e2f76944e7eaff051997a7aaab48b521f28b99ef
Shiblu <faisalalamshiblu@gmail.com>
2019-04-15 18:52:32 +0600

    Oshop Final

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBefYLhJ0ndqHjuei4fQSwf_LErlc4uIDo',
+    authDomain: 'onlineshop-46b5a.firebaseapp.com',
+    databaseURL: 'https://onlineshop-46b5a.firebaseio.com',
+    projectId: 'onlineshop-46b5a',
+    storageBucket: 'onlineshop-46b5a.appspot.com',
+    messagingSenderId: '423785713496'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBefYLhJ0ndqHjuei4fQSwf_LErlc4uIDo',
+    authDomain: 'onlineshop-46b5a.firebaseapp.com',
+    databaseURL: 'https://onlineshop-46b5a.firebaseio.com',
+    projectId: 'onlineshop-46b5a',
+    storageBucket: 'onlineshop-46b5a.appspot.com',
+    messagingSenderId: '423785713496'
+  }
+};
